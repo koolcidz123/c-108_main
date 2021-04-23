4cd818cc42f739c1053e44a79ecaf4636788ac28
koolcidz123 <67968678+koolcidz123@users.noreply.github.com>
2021-04-23 19:38:27 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import random
+import pandas as pd
+import plotly.express as px
+import plotly.figure_factory as ff
+import csv
+
+df = pd.read_csv("data.csv")
+
+# HEIGHT-
+height = df["Height(Inches)"].tolist()
+fig = ff.create_distplot([height], ["height"], show_hist= False )
+fig.show()
+
+# WEIGHT -
+weight = df["Weight(Pounds)"].tolist()
+fig1 = ff.create_distplot([weight], ["weight"], show_hist= False)
+fig1.show()
+# print(df)
+
